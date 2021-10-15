d14b18b0ed7c2ddc9769500dbb6a2e35750ddb15
Your Name <you@example.com>
2021-10-15 16:46:23 +0200

    miss interface

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myLibrary = [{title:'Nicola e i cavalieri', author:'Nicola De Sanctis', pages:110, read:true},
-{title:'Nicola e i cavalieri', author:'Nicola De Sanctis', pages:110, read:true}];
+{title:'Nicola e i cavalieri', author:'Nicola De Sanctis', pages:110, read:true},];
 
 display(myLibrary);
 
@@ -14,6 +14,87 @@ newBook.addEventListener('click', () => {
     const form = document.createElement('div');
     form.classList.add('form');
     desktop.appendChild(form);
+    
+    const formhtml = document.createElement('form');
+    formhtml.classList.add('.formhtml');
+    
+    const divTitle = document.createElement('div');
+    const inputTitle = document.createElement('input');
+    inputTitle.setAttribute('type','text');
+    inputTitle.setAttribute('id','title');
+    const titleLabel = document.createElement('label');
+    titleLabel.setAttribute('for', 'title');
+    titleLabel.textContent='Title'
+    divTitle.appendChild(titleLabel);
+    divTitle.appendChild(inputTitle);
+    formhtml.appendChild(divTitle);
+
+    const divAuthor = document.createElement('div');
+    const inputAuthor = document.createElement('input');
+    inputAuthor.setAttribute('type','text');
+    inputAuthor.setAttribute('id','author');
+    const authorLabel = document.createElement('label');
+    authorLabel.setAttribute('for', 'author');
+    authorLabel.textContent='Author';
+    divAuthor.appendChild(authorLabel);
+    divAuthor.appendChild(inputAuthor);
+    formhtml.appendChild(divAuthor);
+
+    const divPages = document.createElement('div');
+    const inputPages = document.createElement('input');
+    inputPages.setAttribute('type','text');
+    inputPages.setAttribute('id','pages');
+    const pagesLabel = document.createElement('label');
+    pagesLabel.setAttribute('for', 'author');
+    pagesLabel.textContent='Pages';
+    divPages.appendChild(pagesLabel);
+    divPages.appendChild(inputPages);
+    formhtml.appendChild(divPages);
+
+    const divRead = document.createElement('div');
+    const inputRead = document.createElement('input');
+    inputRead.setAttribute('type','checkbox');
+    inputRead.setAttribute('id','read');
+    const readLabel = document.createElement('label');
+    readLabel.setAttribute('for', 'read');
+    readLabel.textContent='Already read';
+    divRead.appendChild(readLabel);
+    divRead.appendChild(inputRead);
+    formhtml.appendChild(divRead);
+
+    const divSubmit = document.createElement('div');
+    const submitButton = document.createElement('button');
+    submitButton.setAttribute('class', 'submitbutton');
+    submitButton.textContent = 'Add new book'
+    divSubmit.appendChild(submitButton);
+    const submitButton2 = document.createElement('button');
+    submitButton2.setAttribute('class', 'deletebutton');
+    submitButton2.textContent = 'Cancel'
+    divSubmit.appendChild(submitButton2);
+    
+
+    form.appendChild(formhtml);
+
+    form.appendChild(divSubmit);
+
+    const submittextvalue = document.querySelector('.submitbutton');
+    submittextvalue.addEventListener('click', ()=> {
+        addBookToLibrary(inputTitle.value,inputAuthor.value,inputPages.value,inputRead.checked);
+        displayLast(myLibrary);
+        form.remove();
+        formcontainer.remove();
+
+    })
+
+    const cancelOperation = document.querySelector('.deletebutton');
+    cancelOperation.addEventListener('click', ()=> {
+        form.remove();
+        formcontainer.remove();
+
+    })
+    
+
+
 
 })
 
@@ -56,9 +137,58 @@ newBook.addEventListener('click', () => {
 
 
 
+function displayLast(lista) {
+    const containerBooks = document.querySelector('.containerBooks')
+        const containerBook = document.createElement('div');
+        containerBook.classList.add('containerBook');
+        
+        const titleContainer = document.createElement('div');
+        titleContainer.classList.add('titleContainer');
+        titleContainer.textContent=lista[(lista.length-1)].title;
+        containerBook.appendChild(titleContainer);
+        
+        const authorContainer = document.createElement('div');
+        authorContainer.classList.add('authorContainer');
+        authorContainer.textContent=lista[(lista.length-1)].author;
+        containerBook.appendChild(authorContainer);
+
+        const pagesContainer = document.createElement('div');
+        pagesContainer.classList.add('pagesContainer');
+        pagesContainer.textContent=lista[(lista.length-1)].pages;
+        containerBook.appendChild(pagesContainer);
+
+        const readContainer = document.createElement('div');
+        readContainer.classList.add('readContainer');
+        readContainer.textContent=lista[(lista.length-1)].read;
+        containerBook.appendChild(readContainer);
+
+        const removeBook = document.createElement('button');
+        removeBook.classList.add('removeBook');
+        removeBook.textContent='✕';
+        containerBook.appendChild(removeBook);
+
+        removeBook.addEventListener('click', ()=> {
+            lista.pop();
+            containerBook.remove();
+        })
+
+        const readStatus = document.createElement('button');
+        readStatus.classList.add('readStatus');
+        readStatus.textContent='Read';
+        containerBook.appendChild(readStatus);
+
+        readStatus.addEventListener('click', ()=> {
+            readContainer.textContent=true;
+        })
+        
+
+        
+
+        containerBooks.appendChild(containerBook);
 
 
-
+    
+}
 
 
 
@@ -89,6 +219,25 @@ function display(lista) {
         readContainer.textContent=lista[i].read;
         containerBook.appendChild(readContainer);
 
+        const removeBook = document.createElement('button');
+        removeBook.classList.add('removeBook');
+        removeBook.textContent='✕';
+        containerBook.appendChild(removeBook);
+
+        removeBook.addEventListener('click', ()=> {
+            lista.splice(i,1);
+            containerBook.remove();
+        })
+
+        const readStatus = document.createElement('button');
+        readStatus.classList.add('readStatus');
+        readStatus.textContent='Read';
+        containerBook.appendChild(readStatus);
+
+        readStatus.addEventListener('click', ()=> {
+            readContainer.textContent=true;
+        })
+
         containerBooks.appendChild(containerBook);
 
 
@@ -100,21 +249,8 @@ function display(lista) {
 
 
 
-function addBookToLibrary() {
-    let title = prompt('Title?');
-    let author = prompt('author?');
-    let pages = prompt('pages?');
-    let read = prompt('read?');
-    if(read == 'yes') {
-        read = true;
-    }
-    else if (read == 'no') {
-        read == false;
-    }
-
-    console.log(title,author,pages,read)
+function addBookToLibrary(title,author,pages,read) {
     const book = new Book(title,author,pages,read);
-    console.log(book);
     myLibrary.push(book);
 }
 
